c565acff62b6763bff3a49d9f5f36a4cc44e9a36
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2015-01-05 12:35:46 +0000

    Finish user edit, update, index, and destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+      @users = User.paginate(page: params[:page])
+  end
+
   def show
       @user = User.find params[:id]
   end
@@ -24,15 +32,39 @@ class UsersController < ApplicationController
   private :user_params
 
   def edit
-      @user = User.find params[:id]
   end
 
   def update
-      @user = User.find params[:id]
       if @user.update_attributes(user_params)
-          # 更新に成功した場合を扱う
+          flash[:success] = 'Profile updated'
+          redirect_to @user
       else
           render "edit"
       end
   end
+
+  def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+  end
+
+  # before action
+  def signed_in_user
+      unless signed_in?
+          store_location
+          redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+  end
+  private :signed_in_user
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+      redirect_to(root_path) unless current_user.admin?
+  end
+  private :admin_user
 end
